378812f01ca471257390a81d135aa2bbb5686a54
Hannan Max <48342941+hannanmax@users.noreply.github.com>
2022-12-11 15:33:40 -0500

    Create ContactsTableViewController.swift
    Created Contacts TableView Controller to list all saved contacts

---
+++ ContactsApp/ContactsTableViewController.swift
@@ -0,0 +1,143 @@
+//
+//  ContactsTableViewController.swift
+//  ContactsApp
+//
+//  Created by Hannan Max on 2022-12-11.
+//
+
+import UIKit
+import CoreData
+var Contacts = [Contact]()
+var addContact = AddContactViewController()
+
+class ContactsTableViewController: UITableViewController {
+    
+    var firstload = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        if (firstload) {
+            firstload = false
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context: NSManagedObjectContext = appDelegate.persistentContainer.viewContext
+            let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Contact")
+            
+            do {
+                let results:NSArray = try context.fetch(request) as NSArray
+                for result in results {
+                    let contact = result as! Contact
+                    Contacts.append(contact)
+                }
+            }
+            catch{
+                print("Didn't get any contact")
+            }
+        }
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return Contacts.count
+    }
+    
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ContactCell", for: indexPath)
+
+        // Configure the cell...
+        
+        cell.textLabel?.text = Contacts[indexPath.row].name
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: "editContact", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if(segue.identifier == "editContact") {
+            let indexPath = tableView.indexPathForSelectedRow!
+            
+            let contactDetail = segue.destination as? AddContactViewController
+            
+            let selectedContact: Contact!
+            selectedContact = Contacts[indexPath.row]
+            
+            contactDetail!.selectedContact = selectedContact
+            
+            
+        }
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+           
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
